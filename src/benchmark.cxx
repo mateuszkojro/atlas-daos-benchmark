b0c5ac7d9082e1ea230dade639d84797b5148fe0
Mateusz Kojro <mateusz.maciej.kojro@cern.ch>
2022-03-14 17:56:31 +0100

    Implementing simplest possible benchmark
    Following steps are performed:
    
    1. Connection with DAOS is established
    2. Connection with given DAOS pool is established
    3. Container is created and formated into key-value store
    4. Objects are added to the container using `EventQueue`
    5. Objects are added to the container using simple put functions
    6. Container is destroyed
    7. Connections is closed

--- src/benchmark.cxx
+++ src/benchmark.cxx
@@ -1,20 +1,73 @@
+#include "EventQueue.h"
 #include "Pool.h"
+#include "daos_event.h"
+#include "daos_types.h"
+#include <cctype>
+#include <chrono>
+#include <cstddef>
+#include <exception>
 #include <iostream>
+#include <ratio>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <vector>
 
-int main()
-{
-	DAOS_CHECK(daos_init());
+#define KB(x) 1024 * x
+#define MB(x) 1024 * 1024 * x
 
-	uuid_t pool_uuid; /** pool UUID */
-	DAOS_CHECK(uuid_parse("27511db3-9fc3-4714-b1b3-1827c6a7b96b", pool_uuid));
+using std::chrono::duration_cast;
 
-	Pool pool(pool_uuid);
+int main(int argc, char **argv) {
 
-	auto container = pool.add_container("name");
+  std::string pool_uuid_string = "7b5abd84-4f94-497e-aca3-c5eeca5503f9";
+  if (argc == 2) {
+    pool_uuid_string = argv[1];
+  }
 
-	std::cout << "Created container UUID: " << unparse(container->get_uuid()) << std::endl;
+  DAOS_CHECK(daos_init());
+  {
+    Pool pool(pool_uuid_string);
+    auto container = pool.add_container("kv_put_test");
 
-	pool.remove_container(container);
+    const int inflight = 1000;
+    EventQueue event_queue(inflight);
 
-	DAOS_CHECK(daos_fini());
+    auto object = container->create_kv_object();
+    int number_ok_events_to_save = 1;
+    int event_size = 10; // MB(5);
+
+    try {
+      auto start = std::chrono::high_resolution_clock::now();
+      for (int i = 0; i < number_ok_events_to_save; i++) {
+        object.put_raw(event_queue.get_event(), std::to_string(i).c_str(), "bb",
+                       event_size);
+      }
+      event_queue.wait();
+      auto end = std::chrono::high_resolution_clock::now();
+      std::cout << "Using event queue:\t"
+                << duration_cast<std::chrono::microseconds>(end - start).count()
+                << "us\n";
+
+      start = std::chrono::high_resolution_clock::now();
+      for (int i = 0; i < number_ok_events_to_save; i++) {
+        object.put_raw(NULL,
+                       std::to_string(i + number_ok_events_to_save).c_str(),
+                       "bb", event_size);
+      }
+      end = std::chrono::high_resolution_clock::now();
+      std::cout << "Without event queue:\t"
+                << std::chrono::duration_cast<std::chrono::microseconds>(end -
+                                                                         start)
+                       .count()
+                << "us\n";
+    } catch (std::exception &e) {
+      std::cout << "Error occurred: " << e.what() << std::endl;
+    }
+
+    pool.remove_container(container);
+  }
+  DAOS_CHECK(daos_fini());
+
+  std::cout << "All operations were ok" << std::endl;
 }
