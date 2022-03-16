7e1a6e40d07308f1efa4c34a753e099e06c9ee93
Mateusz Kojro <mateusz.maciej.kojro@cern.ch>
2022-03-16 11:50:33 +0100

    Fixing const correctness and adding `UUID::to_string()` method

--- src/UUID.h
+++ src/UUID.h
@@ -2,15 +2,18 @@
 #define MK_UUID_H
 
 #include <memory>
+#include <string>
 #include <uuid/uuid.h>
 class UUID {
 public:
   UUID(uuid_t uuid);
   UUID(std::string uuid);
+  UUID(UUID&&) = default;
   UUID(const UUID &);
   const UUID &operator=(const UUID &) = delete;
 
-  const char *raw();
+  const char *raw() const;
+  std::string to_string() const;
 
 private:
   uuid_t uuid_;
@@ -21,11 +24,11 @@ UUID::UUID(std::string uuid) { uuid_parse(uuid.c_str(), uuid_); }
 
 UUID::UUID(const UUID &other) { uuid_copy(uuid_, other.uuid_); }
 
-const char *UUID::raw() { return reinterpret_cast<const char *>(uuid_); }
+const char *UUID::raw() const { return reinterpret_cast<const char *>(uuid_); }
 
-static std::string unparse(UUID uuid) {
+std::string UUID::to_string() const {
   char buffer[36 + 1];
-  uuid_unparse((unsigned char *)uuid.raw(), buffer);
+  uuid_unparse((unsigned char *)uuid_, buffer);
   return buffer;
 }
 
