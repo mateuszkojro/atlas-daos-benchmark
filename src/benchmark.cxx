e2a7a64d96584e818a9b53a0331897cb60f8b570
Mateusz Kojro <mateusz.maciej.kojro@cern.ch>
2022-03-16 12:09:21 +0100

    Adding testing of `Array`

--- src/benchmark.cxx
+++ src/benchmark.cxx
@@ -1,3 +1,5 @@
+#include "Array.h"
+#include "Container.h"
 #include "EventQueue.h"
 #include "Pool.h"
 #include "daos_event.h"
@@ -17,6 +19,65 @@
 #define MB(x) 1024 * 1024 * x
 
 using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::microseconds;
+
+const int number_ok_events_to_save = 100;
+const int event_size = CELL_SIZE; // MB(5);
+
+void test_kv(ContainerPtr &container, EventQueue &event_queue) {
+  auto object = container->create_kv_object();
+
+  auto start = high_resolution_clock::now();
+  for (int i = 0; i < number_ok_events_to_save; i++) {
+    object.put_raw(event_queue.get_event(), std::to_string(i).c_str(), "bb",
+                   event_size);
+  }
+  event_queue.wait();
+  auto end = high_resolution_clock::now();
+  std::cout << "Using event queue:\t"
+            << duration_cast<microseconds>(end - start).count() << "us\n";
+
+  start = high_resolution_clock::now();
+  for (int i = 0; i < number_ok_events_to_save; i++) {
+    object.put_raw(NULL, std::to_string(i + number_ok_events_to_save).c_str(),
+                   "bb", event_size);
+  }
+  end = high_resolution_clock::now();
+  std::cout << "Without event queue:\t"
+            << duration_cast<microseconds>(end - start).count() << "us\n";
+}
+
+void test_array(ContainerPtr &container, EventQueue &event_queue) {
+  auto object = container->create_array();
+
+  char array[CELL_SIZE];
+  array[0] = 'A';
+
+  auto start = high_resolution_clock::now();
+  for (int i = 0; i < number_ok_events_to_save; i++) {
+    object.write_raw(i, array);
+  }
+  auto end = high_resolution_clock::now();
+  std::cout << "Array w/o eq:\t"
+            << duration_cast<microseconds>(end - start).count() << "us"
+            << std::endl;
+
+
+  start = high_resolution_clock::now();
+  for (int i = 0; i < number_ok_events_to_save; i++) {
+    object.write_raw(i, array, event_queue.get_event());
+  }
+  event_queue.wait();
+  end = high_resolution_clock::now();
+  std::cout << "Array using eq:\t"
+            << duration_cast<microseconds>(end - start).count() << "us"
+            << std::endl;
+
+  // for(int i = 0 ; i < number_ok_events_to_save; i++){
+  //   object.read_raw(i);
+  // }
+}
 
 int main(int argc, char **argv) {
 
@@ -28,46 +89,34 @@ int main(int argc, char **argv) {
   DAOS_CHECK(daos_init());
   {
     Pool pool(pool_uuid_string);
-    auto container = pool.add_container("kv_put_test");
+    auto kv_container = pool.add_container();
+    auto array_container = pool.add_container();
 
     const int inflight = 1000;
     EventQueue event_queue(inflight);
 
-    auto object = container->create_kv_object();
-    int number_ok_events_to_save = 1;
-    int event_size = 10; // MB(5);
-
+    std::cout << "--- Testing kv store" << std::endl;
     try {
-      auto start = std::chrono::high_resolution_clock::now();
-      for (int i = 0; i < number_ok_events_to_save; i++) {
-        object.put_raw(event_queue.get_event(), std::to_string(i).c_str(), "bb",
-                       event_size);
-      }
-      event_queue.wait();
-      auto end = std::chrono::high_resolution_clock::now();
-      std::cout << "Using event queue:\t"
-                << duration_cast<std::chrono::microseconds>(end - start).count()
-                << "us\n";
-
-      start = std::chrono::high_resolution_clock::now();
-      for (int i = 0; i < number_ok_events_to_save; i++) {
-        object.put_raw(NULL,
-                       std::to_string(i + number_ok_events_to_save).c_str(),
-                       "bb", event_size);
-      }
-      end = std::chrono::high_resolution_clock::now();
-      std::cout << "Without event queue:\t"
-                << std::chrono::duration_cast<std::chrono::microseconds>(end -
-                                                                         start)
-                       .count()
-                << "us\n";
+      test_kv(kv_container, event_queue);
+      std::cout << "kv_test - succes!" << std::endl;
     } catch (std::exception &e) {
       std::cout << "Error occurred: " << e.what() << std::endl;
+    } catch (...) {
+      std::cout << "Unknown error occurred" << std::endl;
     }
+    std::cout << std::endl << std::endl;
 
-    pool.remove_container(container);
+    std::cout << "--- Testing array store" << std::endl;
+    try {
+      test_array(array_container, event_queue);
+      std::cout << "array_test - succes!" << std::endl;
+    } catch (std::exception &e) {
+      std::cout << "Error occurred: " << e.what() << std::endl;
+    } catch (...) {
+      std::cout << "Unknown error occurred" << std::endl;
+    }
+    pool.remove_container(kv_container);
+    pool.remove_container(array_container);
   }
   DAOS_CHECK(daos_fini());
-
-  std::cout << "All operations were ok" << std::endl;
 }
