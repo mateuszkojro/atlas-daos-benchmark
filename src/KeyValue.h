35db340739d6d9563d550c19797c56ad8d0cd827
Mateusz Kojro <mateusz.maciej.kojro@cern.ch>
2022-03-17 09:58:21 +0100

    Adding result check in `KeyValue::get_raw` and defaulting the dtor

--- src/KeyValue.h
+++ src/KeyValue.h
@@ -19,7 +19,7 @@ public:
   KeyValue(const KeyValue &) = default;
   KeyValue &operator=(KeyValue &&) = default;
   KeyValue &operator=(const KeyValue &) = default;
-  ~KeyValue();
+  ~KeyValue() = default;
 
   void put_raw(daos_event_t *event, const char *key, const char *value,
                size_t value_size);
@@ -33,27 +33,27 @@ private:
 KeyValue::KeyValue(daos_handle_t object_handle, daos_obj_id_t object_id)
     : DAOSObject(object_handle, object_id) {}
 
-KeyValue::~KeyValue() {}
-
 std::string random_string() {
   static std::size_t index = 0;
   index++;
   return std::to_string(index);
 }
 
-void KeyValue::put_raw(daos_event_t *event, const char *key,
-                       const char *value, size_t value_size) {
+void KeyValue::put_raw(daos_event_t *event, const char *key, const char *value,
+                       size_t value_size) {
   DAOS_CHECK(daos_kv_put(object_handle_, DAOS_TX_NONE, 0, key, value_size,
                          value, event));
 }
 
-void KeyValue::change_value_raw() { throw std::runtime_error("Not implemented"); }
+void KeyValue::change_value_raw() {
+  throw std::runtime_error("Not implemented");
+}
 
 void KeyValue::get_raw(daos_event_t *event, const char *key) {
   size_t buffer_size = 128;
   char value[128];
-  daos_kv_get(object_handle_, DAOS_TX_NONE, 0, key, &buffer_size, &value,
-              event);
+  DAOS_CHECK(daos_kv_get(object_handle_, DAOS_TX_NONE, 0, key, &buffer_size,
+                         &value, event));
 
   std::cout << "Value: " << value << std::endl;
 }
