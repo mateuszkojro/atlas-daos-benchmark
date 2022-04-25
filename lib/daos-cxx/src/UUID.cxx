9a517fbe2ac646584e26e43f39d751d412930c54
Mateusz Maciej Kojro <mateusz.maciej.kojro@cern.ch>
2022-04-25 10:47:13 +0200

    Fixing casting in UUID wraper

--- lib/daos-cxx/src/UUID.cxx
+++ lib/daos-cxx/src/UUID.cxx
@@ -6,8 +6,8 @@ UUID::UUID(std::string uuid) { uuid_parse(uuid.c_str(), uuid_); }
 
 UUID::UUID(const UUID& other) { uuid_copy(uuid_, other.uuid_); }
 
-const unsigned char * UUID::raw() const { 
-  return static_cast<const unsigned char *>(uuid_);
+const char * UUID::raw() const { 
+  return reinterpret_cast<const char*>(uuid_);
 }
 
 std::string UUID::to_string() const {
